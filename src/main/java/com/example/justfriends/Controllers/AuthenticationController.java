99eb28659cc853709462a611de829371dee25728
Michael Troia <troia.michael.a@gmail.com>
2021-01-12 15:01:46 -0500

    feat: password hashing and authentication setup for users, removed Group and GroupPost models and tables from database

---
+++ src/main/java/com/example/justfriends/Controllers/AuthenticationController.java
@@ -0,0 +1,13 @@
+package com.example.justfriends.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthenticationController {
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "user/login";
+    }
+
+}
--- src/main/java/com/example/justfriends/Models/User.java
+++ src/main/java/com/example/justfriends/Models/User.java
@@ -41,17 +41,6 @@ public class User {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "friend")
     private List<UserFriend> userFriends;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
-    private List<Group> groupsOwned;
-
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(
-            name = "users_groups",
-            joinColumns = {@JoinColumn(name = "user_id")},
-            inverseJoinColumns = {@JoinColumn(name = "group_id")}
-    )
-    private List<Group> groupsAMemberOf;
-
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private List<Picture> pictures;
 
@@ -59,14 +48,12 @@ public class User {
 
     public User(String aboutMe, Date birthday, String email, String firstName, String password,
                 String lastName, String job, String profile_picture_url, String username,
-                List<UserFriend> userFriends, List<Group> groupsOwned, List<Group> groupsAMemberOf, List<Picture> pictures){
+                List<UserFriend> userFriends, List<Picture> pictures){
         this.birthday = birthday;
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
         this.profile_picture_url = profile_picture_url;
-        this.groupsAMemberOf = groupsAMemberOf;
-        this.groupsOwned = groupsOwned;
         this.userFriends = userFriends;
         this.job = job;
         this.username = username;
@@ -77,21 +64,34 @@ public class User {
 
     public User(long id, String aboutMe, Date birthday, String email, String firstName, String password,
                 String lastName, String job, String profile_picture_url, String username,
-                List<UserFriend> userFriends, List<Group> groupsOwned, List<Group> groupsAMemberOf, List<Picture> pictures){
+                List<UserFriend> userFriends, List<Picture> pictures){
         this.aboutMe = aboutMe;
         this.birthday = birthday;
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
         this.profile_picture_url = profile_picture_url;
-        this.groupsAMemberOf = groupsAMemberOf;
-        this.groupsOwned = groupsOwned;
         this.userFriends = userFriends;
         this.job = job;
         this.username = username;
         this.pictures = pictures;
         this.id = id;
         this.password = password;
+    }
+
+    public User(User copy) {
+        aboutMe = copy.aboutMe;
+        birthday = copy.birthday;
+        email = copy.email;
+        firstName = copy.firstName;
+        lastName = copy.lastName;
+        profile_picture_url = copy.profile_picture_url;
+        userFriends = copy.userFriends;
+        job = copy.job;
+        username = copy.username;
+        pictures = copy.pictures;
+        id = copy.id;
+        password = copy.password;
     }
 
     public long getId(){return id;}
@@ -104,8 +104,6 @@ public class User {
     public String getJob(){return job;}
     public String getProfile_picture_url(){return profile_picture_url;}
     public List<UserFriend> getUserFriends(){return userFriends;}
-    public List<Group> getGroupsOwned(){return groupsOwned;}
-    public List<Group> getGroupsAMemberOf(){return groupsAMemberOf;}
     public List<Picture> getPictures(){return pictures;}
     public String getPassword(){return password;}
 
@@ -119,8 +117,6 @@ public class User {
     public void setJob(String job){this.job = job;}
     public void setProfile_picture_url(String profile_picture_url){this.profile_picture_url = profile_picture_url;}
     public void setUserFriends(List<UserFriend> userFriends){this.userFriends = userFriends;}
-    public void setGroupsOwned(List<Group> groupsOwned){this.groupsOwned = groupsOwned;}
-    public void setGroupsAMemberOf(List<Group> groupsAMemberOf){this.groupsAMemberOf = groupsAMemberOf;}
     public void setPictures(List<Picture> pictures){this.pictures = pictures;}
     public void setPassword(String password){this.password = password;}
 }
