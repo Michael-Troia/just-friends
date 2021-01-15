80fbd721598ca5dc8befff1aa18d7efefbe84e8f
Michael Troia <troia.michael.a@gmail.com>
2021-01-14 21:09:40 -0500

    Comment body changed to TEXT from VARCHAR

--- src/main/java/com/example/justfriends/Models/Comment.java
+++ src/main/java/com/example/justfriends/Models/Comment.java
@@ -10,7 +10,7 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String body;
 
     @Column(nullable = false)
