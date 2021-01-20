adc1b4814cf7815defef96de99ea9bffeaf651dc
Michael-Troia <troia.michael.a@gmail.com>
2021-01-20 10:12:16 -0500

    feat: users can view a list of current users and add a userFriend with a pending status

---
+++ src/main/java/com/example/justfriends/Controllers/UserFriendController.java
@@ -0,0 +1,63 @@
+package com.example.justfriends.Controllers;
+
+import com.example.justfriends.Models.Post;
+import com.example.justfriends.Models.Status;
+import com.example.justfriends.Models.User;
+import com.example.justfriends.Models.UserFriend;
+import com.example.justfriends.Repositories.*;
+import org.springframework.security.core.parameters.P;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserFriendController {
+
+    public UserRepo userRepo;
+    public UserFriendRepo userFriendRepo;
+    public PostRepo postRepo;
+    public CommentRepo commentRepo;
+    public PictureRepo pictureRepo;
+    public GalleryRepo galleryRepo;
+
+    public UserFriendController(UserRepo userRepo, UserFriendRepo userFriendRepo,
+                                PostRepo postRepo, CommentRepo commentRepo,
+                          PictureRepo pictureRepo, GalleryRepo galleryRepo) {
+        this.userRepo = userRepo;
+        this.userFriendRepo = userFriendRepo;
+        this.postRepo = postRepo;
+        this.commentRepo = commentRepo;
+        this.galleryRepo = galleryRepo;
+        this.pictureRepo = pictureRepo;
+    }
+    //create UserFriend
+    @GetMapping("/{username}/friends")
+    public String showUsers(Model model,
+                            @PathVariable String username) {
+        List<User> userList = userRepo.findAllByIdGreaterThan(0);
+        model.addAttribute("userList", userList);
+
+        return "userFriend/create";
+    }
+
+    @PostMapping("/{username}/{friendName}")
+    public String addFriend(@PathVariable String username,
+                            @PathVariable String friendName){
+        UserFriend userFriend = new UserFriend();
+        userFriend.setFriend(userRepo.findByUsername(friendName));
+        userFriend.setStatus(Status.PENDING);
+        userFriend.setUser(userRepo.findByUsername(username));
+        UserFriend db_userFriend = userFriendRepo.save(userFriend);
+
+        return "redirect:/";
+    }
+
+
+}
--- src/main/java/com/example/justfriends/Repositories/UserRepo.java
+++ src/main/java/com/example/justfriends/Repositories/UserRepo.java
@@ -3,6 +3,9 @@ package com.example.justfriends.Repositories;
 import com.example.justfriends.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepo extends JpaRepository <User, Long> {
     User findByUsername(String username);
+    List<User> findAllByIdGreaterThan(long id);
 }
